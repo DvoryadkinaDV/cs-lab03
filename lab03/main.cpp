a760b9f95309e2323f328858f9c049818a18abc2
Diana <ddvoradkina@gmail.com>
2021-06-03 22:23:03 +0300

    Initial Commit

---
+++ lab03/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab03
+//
+//  Created by Диана on 03.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
